ffd5317604c5a43f5cc08e346aa956c9e520d48f
MinhChi <triedmlaunchpad@gmail.com>
2024-07-08 22:46:57 +0700

    [Update] review 3

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-dotenv = require('dotenv').config();
-var parse = require('parse-link-header');
-const PER_PAGE = 100
+dotenv = require("dotenv").config();
+var parse = require("parse-link-header");
+const PER_PAGE = 100;
 
-
-class ResponseGitHub {
-    constructor(nameRepo=null, nameOwner=null, token=null) {
+class RepoGitHub {
+    constructor(nameRepo = null, nameOwner = null, token = null) {
         this.nameRepo = nameRepo;
         this.nameAuthor = nameOwner;
         this.headers = {
-            'X-GitHub-Api-Version': '2022-11-28',
-            'accept': 'application/vnd.github+json',
-            'Authorization': `token ${token}`
-        }
+            "X-GitHub-Api-Version": "2022-11-28",
+            accept: "application/vnd.github+json",
+            Authorization: `token ${token}`,
+        };
+        this.urlCommit = `https://api.github.com/repos/${this.nameAuthor}/${this.nameRepo}/commits`;
+        this.commits = [new Commit()];
     }
 
     getNameRepo() {
@@ -23,105 +24,112 @@ class ResponseGitHub {
     }
 
     getHeaders() {
-        return this.headers
+        return this.headers;
     }
 
     getCommitUrl(per_page, page) {
-        return `https://api.github.com/repos/${this.nameAuthor}/${this.nameRepo}/commits?per_page=${per_page}&page=${page}`
-    }
-}
-
-class CommitGithub extends ResponseGitHub {
-    async getReponse(per_page, page) {
-        const response = await fetch(
-            this.getCommitUrl(per_page, page),
-            {   
-                method: 'GET',
-                headers: this.headers
-            })
-        return response
+        return `${this.urlCommit}?per_page=${per_page}&page=${page}`;
     }
 
     async getAllCommits() {
         try {
-            let page = 1
-            
-            const response = await this.getReponse(PER_PAGE, page)
-            const allCommits = await response.json();
-            const result = {
-                commits: allCommits.map(commit => ({
-                    author: commit.commit.author.name,
-                    message: commit.commit.message
-                }))
-            };
+            let page = 1;
+            const response = await fetch(this.getCommitUrl(PER_PAGE, page), {
+                method: "GET",
+                headers: this.headers,
+            });
 
-            const lastPage = parse(response.headers.get('link')).last.page
+            const data_json = await response.json();
+            let result = data_json.map(
+                (commit) =>
+                    new Commit(commit.commit.author.name, commit.commit.message)
+            );
+
+            const lastPage = parse(response.headers.get("link")).last.page;
             if (!lastPage) {
-                return result;
+                return this.commits = result;
             }
 
-            page += 1
-            for (page; page <= lastPage; page++) {
-                const response = await this.getReponse(PER_PAGE, page)
-                const allCommits = await response.json();
+            for (page = 2; page <= lastPage; page++) {
+                const response = await fetch(
+                    this.getCommitUrl(PER_PAGE, page),
+                    {
+                        method: "GET",
+                        headers: this.headers,
+                    }
+                );
 
-                result.commits = result.commits.concat(allCommits.map(commit => ({
-                    author: commit.commit.author.name,
-                    message: commit.commit.message
-                })))
+                const data_json = await response.json();
+                
+                result = result.concat(
+                    data_json.map(
+                        (commit) =>
+                            new Commit(
+                                commit.commit.author.name,
+                                commit.commit.message
+                            )
+                    )
+                );
             }
 
-            return result;
+            return this.commits = result;
         } catch (error) {
             console.log(error);
         }
     }
+}
 
-    async getUniqueUsers() {
-        const allCommits = await this.getAllCommits();
-        const uniqueUsers = [];
-        allCommits.commits.forEach(commit => {
-            if (!uniqueUsers.includes(commit.author)) {
-                uniqueUsers.push(commit.author);
-            }
-        });
-
-        return uniqueUsers;
-    }
-
-    async getMostUserCommits() {
-        const allCommits = await this.getAllCommits();
-        const users = {};
-        let maxUser = '';
-        let maxCommits = 0;
-
-        allCommits.commits.forEach(commit => {
-            if (users[commit.author]) {
-                users[commit.author] += 1;
-            } else {
-                users[commit.author] = 1;
-            }
-        });
-
-        for (const user in users) {
-            if (users[user] > maxCommits) {
-                maxUser = user;
-                maxCommits = users[user];
-            }
-        }
-
-        return maxUser;
+class Commit {
+    constructor(author = null, message = null) {
+        this.author = author;
+        this.message = message;
     }
 }
 
-async function main() {
-    const response = new CommitGithub('swift-stress-tester', 'swiftlang', process.env.GITHUB_TOKEN);
-    const allcommits = await response.getAllCommits();
-    const uniqueUsers = await response.getUniqueUsers();
-    const mostUserCommits = await response.getMostUserCommits();
+async function getUniqueUsers(listCommits) {
+    let users = [];
+    listCommits.forEach((commit) => {
+        if (!users.includes(commit.author)) {
+            users.push(commit.author);
+        }
+    });
+    return users;
+}
 
-    console.log(`All commits:`,allcommits);
-    console.log(`Number of commits:`, allcommits.commits.length);
+async function getMostUserCommits(listCommits) {
+    let count = {};
+    listCommits.forEach((commit) => {
+        if (!count[commit.author]) {
+            count[commit.author] = 1;
+        } else {
+            count[commit.author]++;
+        }
+    });
+
+    let max = 0;
+    let user = "";
+    for (let key in count) {
+        if (count[key] > max) {
+            max = count[key];
+            user = key;
+        }
+    }
+
+    return {user: user, count: max};
+}
+
+async function main() {
+    const repo = new RepoGitHub(
+        "swift-stress-tester",
+        "swiftlang",
+        process.env.GITHUB_TOKEN
+    );
+    const allcommits = await repo.getAllCommits();
+    const uniqueUsers = await getUniqueUsers(allcommits);
+    const mostUserCommits = await getMostUserCommits(allcommits);
+
+    console.log(`All commits:`, allcommits);
+    console.log(`Number of commits:`, allcommits.length);
     console.log(`Users commit:`,uniqueUsers)
     console.log(`Most user commit:`, mostUserCommits)
 }
